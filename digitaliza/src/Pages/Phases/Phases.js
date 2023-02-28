2fe22bc279b66d4466f2fa855bea997bfc78b70c
Nathan <nathan.ansayc@gmail.com>
2023-02-28 16:52:24 -0300

    Alterações progress bar

--- digitaliza/src/Pages/Phases/Phases.js
+++ digitaliza/src/Pages/Phases/Phases.js
@@ -1,23 +1,27 @@
 import './Phases.css'
 
 function Phases(){
+    const max = 5
+    const value = 2
+    let rows = [[],[]]
+    for(let i = 0; i<max;i++){
+        if(i<=value){
+            rows[0].push(<div className='progress-step-active'></div>)
+        }
+        else{
+            rows[0].push(<div className='progress-step'></div>)
+        }
+        rows[1].push(<h2>Item {i}</h2>)
+    }
+
     return(
         <div className='phase-card'>
-            <h1>Processo Seletivo</h1>
             <div className='progressbar'>
-                <progress id="file" value ="2"max="4"></progress>
-                <div className='progress-step'></div>
-                <div className='progress-step'></div>
-                <div className='progress-step'></div>
-                <div className='progress-step'></div>
-                <div className='progress-step'></div>
+                <progress id="file" value ={value} max={max-1}></progress>
+                {rows[0]}
             </div>
             <div className='description'>
-                <h2>nathan</h2>
-                <h2>nathan</h2>
-                <h2>nathan</h2>
-                <h2>nathan</h2>
-                <h2>nathan</h2>
+                {rows[1]}
             </div>
         </div>
     )
